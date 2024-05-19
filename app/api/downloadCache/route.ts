3937e7441344e26b6b165238eab7559a123f6add
lakmalmabm <lakmalmabm@gmail.com>
2024-05-19 23:00:29 +0530

    cashe update

--- app/api/downloadCache/route.ts
+++ app/api/downloadCache/route.ts
@@ -4,40 +4,40 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const cacheFolder = "./cache";
+    // const cacheFolder = "./cache";
     
-    // Check if cache folder exists, create it if not
-    if (!fs.existsSync(cacheFolder)) {
-      fs.mkdirSync(cacheFolder);
-    }
+    // // Check if cache folder exists, create it if not
+    // if (!fs.existsSync(cacheFolder)) {
+    //   fs.mkdirSync(cacheFolder);
+    // }
     
-    // Get list of files from Supabase bucket
-    const { data: files, error } = await supabase.storage.from("cache").list();
-    if (error) {
-      throw error;
-    }
+    // // Get list of files from Supabase bucket
+    // const { data: files, error } = await supabase.storage.from("cache").list();
+    // if (error) {
+    //   throw error;
+    // }
 
-    // Download each file from Supabase bucket to the cache folder
-    for (const file of files) {
-      const filePath = `${cacheFolder}/${file.name}`;
-      const { data: fileData, error: downloadError } = await supabase.storage
-        .from("cache")
-        .download(file.name);
+    // // Download each file from Supabase bucket to the cache folder
+    // for (const file of files) {
+    //   const filePath = `${cacheFolder}/${file.name}`;
+    //   const { data: fileData, error: downloadError } = await supabase.storage
+    //     .from("cache")
+    //     .download(file.name);
       
-      if (downloadError) {
-        console.error(`Error downloading file ${file.name} from Supabase:`, downloadError.message);
-      } else {
-        const fileStream = fs.createWriteStream(filePath);
-        fileStream.write(Buffer.from(await fileData.arrayBuffer()));
-        fileStream.end();
-        console.log(`File ${file.name} downloaded successfully to ${cacheFolder}.`);
-      }
-    }
+    //   if (downloadError) {
+    //     console.error(`Error downloading file ${file.name} from Supabase:`, downloadError.message);
+    //   } else {
+    //     const fileStream = fs.createWriteStream(filePath);
+    //     fileStream.write(Buffer.from(await fileData.arrayBuffer()));
+    //     fileStream.end();
+    //     console.log(`File ${file.name} downloaded successfully to ${cacheFolder}.`);
+    //   }
+    // }
 
-    return NextResponse.json(
-      { success: true, message: "Downloaded files from Supabase successfully." },
-      { status: 200 },
-    );
+    // return NextResponse.json(
+    //   { success: true, message: "Downloaded files from Supabase successfully." },
+    //   { status: 200 },
+    // );
   } catch (error) {
     console.error("Error downloading files from Supabase:", error);
     return NextResponse.json(
